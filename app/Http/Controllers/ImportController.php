17b7fc6c41ce6123c2cf4bcdc896c4b699206e05
Clebien <56209125+Clebien@users.noreply.github.com>
2020-05-11 17:18:17 +0200

    Update ImportController.php

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Imports\importIndividu;
+use App\Imports\ImportIndividu;
 use App\Imports\ImportGroupe;
 use Maatwebsite\Excel\Facades\Excel;
 use DB;
